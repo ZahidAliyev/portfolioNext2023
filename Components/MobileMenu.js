9b803e230158a5c36b44b7cdad432bc26b501615
Zahid <hermitaliyev96@gmail.com>
2023-01-17 18:30:28 +0400

    Mob Menu Icon logic

--- Components/MobileMenu.js
+++ Components/MobileMenu.js
@@ -1,123 +1,8 @@
-import cn from 'classnames';
-import Link from 'next/link';
-import useDelayedRender from 'use-delayed-render';
-import { useState, useEffect } from 'react';
-import styles from '../styles/mobile-menu.module.scss';
-
-export default function MobileMenu() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
-    isMenuOpen,
-    {
-      enterDelay: 20,
-      exitDelay: 300
-    }
-  );
-
-  function toggleMenu() {
-    if (isMenuOpen) {
-      setIsMenuOpen(false);
-      document.body.style.overflow = '';
-    } else {
-      setIsMenuOpen(true);
-      document.body.style.overflow = 'hidden';
-    }
-  }
-
-  useEffect(() => {
-    return function cleanup() {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
-  return (
-    <>
-      <button
-        className={cn(styles.burger)}
-        aria-label="Toggle menu"
-        type="button"
-        onClick={toggleMenu}
-      >
-        <MenuIcon data-hide={isMenuOpen} />
-        <CrossIcon data-hide={!isMenuOpen} />
-      </button>
-      {isMenuMounted && (
-        <ul
-          className={cn(
-            styles.menu,
-            isMenuRendered && styles.menuRendered
-          )}
-        >
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '150ms' }}
-          >
-            <Link href="/" className={styles.link}>
-              Home
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '175ms' }}
-          >
-            <Link href="/guestbook" className={styles.link}>
-              Guestbook
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '200ms' }}
-          >
-            <Link href="/dashboard" className={styles.link}>
-              Dashboard
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '250ms' }}
-          >
-            <Link href="/blog" className={styles.link}>
-              Blog
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '275ms' }}
-          >
-            <Link href="/snippets" className={styles.link}>
-              Snippets
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '300ms' }}
-          >
-            <Link href="/newsletter" className={styles.link}>
-              Newsletter
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '325ms' }}
-          >
-            <Link href="/tweets" className={styles.link}>
-              Tweets
-            </Link>
-          </li>
-          <li
-            className={styles.listItem}
-            style={{ transitionDelay: '350ms' }}
-          >
-            <Link href="/uses" className={styles.link}>
-              Uses
-            </Link>
-          </li>
-        </ul>
-      )}
-    </>
-  );
-}
-
+import cn from "classnames";
+import Link from "next/link";
+import useDelayedRender from "use-delayed-render";
+import { useState, useEffect } from "react";
+import styles from "../styles/MobileMenu.module.scss";
 function MenuIcon(props) {
   return (
     <svg
@@ -164,5 +49,167 @@ function CrossIcon(props) {
       <path d="M18 6L6 18" />
       <path d="M6 6l12 12" />
     </svg>
+  );
+}
+// export default function MobileMenu() {
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+//   const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
+//     isMenuOpen,
+//     {
+//       enterDelay: 20,
+//       exitDelay: 300
+//     }
+//   );
+
+//   function toggleMenu() {
+//     if (isMenuOpen) {
+//       setIsMenuOpen(false);
+//       document.body.style.overflow = '';
+//     } else {
+//       setIsMenuOpen(true);
+//       document.body.style.overflow = 'hidden';
+//     }
+//   }
+
+//   useEffect(() => {
+//     return function cleanup() {
+//       document.body.style.overflow = '';
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       <button
+//         className={cn(styles.burger)}
+//         aria-label="Toggle menu"
+//         type="button"
+//         onClick={toggleMenu}
+//       >
+//         <MenuIcon data-hide={isMenuOpen} />
+//         <CrossIcon data-hide={!isMenuOpen} />
+//       </button>
+//       {isMenuMounted && (
+//         <ul
+//           className={cn(
+//             styles.menu,
+//             isMenuRendered && styles.menuRendered
+//           )}
+//         >
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '150ms' }}
+//           >
+//             <Link href="/" className={styles.link}>
+//               Home
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '175ms' }}
+//           >
+//             <Link href="/guestbook" className={styles.link}>
+//               Guestbook
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '200ms' }}
+//           >
+//             <Link href="/dashboard" className={styles.link}>
+//               Dashboard
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '250ms' }}
+//           >
+//             <Link href="/blog" className={styles.link}>
+//               Blog
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '275ms' }}
+//           >
+//             <Link href="/snippets" className={styles.link}>
+//               Snippets
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '300ms' }}
+//           >
+//             <Link href="/newsletter" className={styles.link}>
+//               Newsletter
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '325ms' }}
+//           >
+//             <Link href="/tweets" className={styles.link}>
+//               Tweets
+//             </Link>
+//           </li>
+//           <li
+//             className={styles.listItem}
+//             style={{ transitionDelay: '350ms' }}
+//           >
+//             <Link href="/uses" className={styles.link}>
+//               Uses
+//             </Link>
+//           </li>
+//         </ul>
+//       )}
+//     </>
+//   );
+// }
+
+export default function MobileMenu() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    if(isMenuOpen) {
+      setIsMenuOpen((prevState)=> prevState = false);
+    } else {
+      setIsMenuOpen((prevState)=> prevState = true);
+
+    }
+  };
+  return (
+    <>
+      <button
+        className={styles.burger}
+        aria-label="menu toggle"
+        type="button"
+        onClick={toggleMenu}
+      >
+        <svg
+          data-hide={isMenuOpen}
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          strokeLinecap="round"
+        >
+          <path d="M2 7.5 H18" stroke="currentColor" strokeWidth="2" />
+          <path d="M2 14.5 H18" stroke="currentColor" strokeWidth="2" />
+        </svg>
+        <svg
+          data-hide={!isMenuOpen}
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          shapeRendering="geometricPrecision"
+        >
+          <path d="M18 6L6 18" />
+          <path d="M6 6l12 12" />
+        </svg>
+      </button>
+    </>
   );
 }
