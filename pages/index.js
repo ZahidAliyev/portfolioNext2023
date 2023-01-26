48f19ce04491a74bcebfdba94b9ac1992727258c
Zahid <hermitaliyev96@gmail.com>
2023-01-26 17:44:14 +0400

    Basic Blogpost

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Container from "../Components/Container";
 const inter = Inter({ subsets: ["latin"] });
 
 import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -36,6 +37,22 @@ export default function Home() {
               tempor incididunt ut labore et dolore magna aliqua.</p>
 
           </div>
+        </div>
+        <h3>Recent Posts</h3>
+
+        <div className={styles.home_posts_container}>
+          <Link className={styles.blog_post_card} href="/">
+              <div className={styles.blog_post_card__content_container}>
+                <div className={styles.blog_post_card__content_container__header_container}>
+                  <h4 className={styles.blog_post_card__content_container__header}>
+                    What is project management and why you should care
+                  </h4>
+                </div>
+                <div className={styles.blog_post_card__content_container__info_container}>
+                  <h5>Project management</h5>
+                </div>
+              </div>
+          </Link>
         </div>
       </div>
     </Container>
